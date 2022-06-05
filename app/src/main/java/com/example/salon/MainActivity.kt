c3d61c6ae8d6d848764ee9ae2f24d7a01caa3d10
Digital <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>
2022-06-05 19:46:34 +0500

    xdgh

--- app/src/main/java/com/example/salon/MainActivity.kt
+++ app/src/main/java/com/example/salon/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.salon.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         var a=0
         var uid:String? = null
         lateinit var sharedPreferences: SharedPreferences
-        lateinit var editor: SharedPreferences.Editor
+        lateinit var editor:SharedPreferences.Editor
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,15 +35,18 @@ class MainActivity : AppCompatActivity() {
 
         progressDialog.setTitle("Please Wait")
         progressDialog.setMessage("Kuting...")
-        binding.button2.setOnClickListener{
+        binding.button2.setOnClickListener {
+            startActivity(Intent(this, MainActivity2::class.java))
+        }
+        binding.button1.setOnClickListener{
             progressDialog.show()
             firebaseAuth.signInWithEmailAndPassword(
                 binding.edittext1.text.toString(),
                 binding.edittext2.text.toString()
             ).addOnCompleteListener(
-                {
+                OnCompleteListener {
 
-                    if (it.isSuccessful){
+                    if (it.isSuccessful) {
                         Toast.makeText(this,firebaseAuth.currentUser!!.uid,Toast.LENGTH_SHORT).show();
                         uid=firebaseAuth.currentUser!!.uid
                         progressDialog.hide()
@@ -53,16 +57,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun saqlash(){
-        sharedPreferences=getPreferences(MODE_PRIVATE)
-        editor= sharedPreferences.edit()
-        editor.putString("abs123", uid)
+    fun saqlash() {
+        sharedPreferences = getPreferences(MODE_PRIVATE)
+        editor = sharedPreferences.edit()
+        editor.putString("abc123", uid)
         editor.commit()
     }
-    fun oqibolish(){
-        sharedPreferences=getPreferences(MODE_PRIVATE)
-        uid= sharedPreferences.getString("abs123",null).toString()
-        if (uid!=null){
+    fun oqibolish() {
+        sharedPreferences = getPreferences(MODE_PRIVATE)
+        uid = sharedPreferences.getString("abc123",null)
+        if (uid != null) {
             startActivity(Intent(this,MainActivity3::class.java))
         }
     }
